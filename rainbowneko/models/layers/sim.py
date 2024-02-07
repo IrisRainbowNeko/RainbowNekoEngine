7acfb4bbf0e7ad813d87a1237851fa0ccba4178b
7eu7d7 <228218809@qq.com>
2024-02-07 15:47:40 +0800

    bug fix

--- rainbowneko/models/layers/sim.py
+++ rainbowneko/models/layers/sim.py
@@ -30,14 +30,12 @@ class StyleSimilarity(nn.Module):
         self.batch_mean = batch_mean
 
     def forward(self, feat1, feat2):  # feat1: [B,C,H,W]
+        sim = F.mse_loss(self.gram_matrix(feat1), self.gram_matrix(feat2), reduction='none')
+        sim = torch.sigmoid(sim - 10.)
         if self.batch_mean:
-            sim = F.mse_loss(self.gram_matrix(feat1), self.gram_matrix(feat2), reduction='none')
-            sim = torch.sigmoid(sim - 10.)
             return sim.mean(dim=(1, 2))
         else:
-            sim = F.mse_loss(self.gram_matrix(feat1), self.gram_matrix(feat2))
-            sim = torch.sigmoid(sim - 10.)
-            return sim
+            return sim.mean()
 
     @staticmethod
     def gram_matrix(x, should_normalize=True):
