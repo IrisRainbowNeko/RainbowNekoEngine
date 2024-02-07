7a6bc7bb0faa34b0e3423b99778596bf8d2b8af1
7eu7d7 <228218809@qq.com>
2024-02-07 14:04:49 +0800

    bug fix

--- rainbowneko/train/loss/style.py
+++ rainbowneko/train/loss/style.py
@@ -26,7 +26,7 @@ class StyleLoss(LossContainer):
 
             loss_pos = sum(self.style_sim(q, pos) for q, pos in zip(query, positive_key))/len(query)
             loss_neg = sum(self.style_sim(q, neg) for q, neg in zip(query, negative_keys))/len(query)
-            return loss_pos - loss_neg*0.1
+            return loss_pos - torch.clamp(loss_neg, -1e4, 1e4)*0.1
         else:
             query = [item[target_tensor == 0, ...] for item in input_tensor]
             positive_key = [item[target_tensor == 1, ...] for item in input_tensor]
