2278718e8dc37765600f11b0b8c635eac73d24f4
7eu7d7 <228218809@qq.com>
2024-02-23 15:02:43 +0800

    bug fix

--- rainbowneko/evaluate/classification.py
+++ rainbowneko/evaluate/classification.py
@@ -6,4 +6,8 @@ from .base import EvaluatorContainer
 class ClsEvaluatorContainer(EvaluatorContainer):
 
     def update(self, pred: Dict[str, Any], target: Dict[str, Any]):
-        return self.evaluator(pred['pred'], target['label'])
+        if isinstance(pred, list):
+            for pred_cudai, target_cudai in zip(pred['pred'], target['label']):
+                self.metric_list.append(self.evaluator(pred_cudai, target_cudai))
+        else:
+            self.metric_list.append(self.evaluator(pred['pred'], target['label']))
