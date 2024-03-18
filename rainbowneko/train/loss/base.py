60103c57cd5ea481d357f9c005a2c73060921a31
IrisRainbowNeko <228218809@qq.com>
2024-03-18 17:44:55 +0800

    rce+nce

--- rainbowneko/train/loss/base.py
+++ rainbowneko/train/loss/base.py
@@ -1,13 +1,15 @@
 from torch import nn
 
 class LossContainer(nn.Module):
-    def __init__(self, loss, weight=1.0):
+    def __init__(self, loss, weight=1.0, pred_key='pred', target_key='label'):
         super().__init__()
         self.loss = loss
         self.alpha = weight
+        self.pred_key = pred_key
+        self.target_key = target_key
 
     def forward(self, pred, target):
-        return self.loss(pred['pred'], target['label']) * self.alpha
+        return self.loss(pred[self.pred_key], target[self.target_key]) * self.alpha
 
 class LossGroup(nn.Module):
     def __init__(self, loss_list):
--- rainbowneko/train/loss/contrastive.py
+++ rainbowneko/train/loss/contrastive.py
@@ -1,16 +1,17 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-import numpy as np
+from einops import repeat
 
 from .base import LossContainer
+
 
 class MLCEImageLoss(LossContainer):
     def __init__(self, weight=1.0, eps=1e-4, reduction='mean'):
         super().__init__(None, weight=weight)
         self.ce_loss = nn.CrossEntropyLoss()
         self.eps = eps
-        self.reduction=reduction
+        self.reduction = reduction
 
     def forward(self, pred, target):
         input_tensor = pred['pred']  # [B,C]
@@ -31,6 +32,7 @@ class MLCEImageLoss(LossContainer):
         y = torch.arange(0, len(target_tensor)).to(input_tensor.device)
 
         return F.nll_loss(log_prob_x.log(), y, reduction=self.reduction) * self.alpha
+
 
 # Copy from https://github.com/RElbers/info-nce-pytorch/blob/main/info_nce/__init__.py
 class InfoNCELoss(LossContainer):
@@ -85,84 +87,69 @@ class InfoNCELoss(LossContainer):
         input_tensor = pred['feat'][0]  # [B,C]
         target_tensor = target['label']
 
-        if 'label_neg' in target: # 显式给出pos和neg
-            query = input_tensor[target_tensor==0, :]
-            positive_key = input_tensor[target_tensor==1, :]
-            negative_keys = input_tensor[target_tensor==2, :]
+        if 'label_neg' in target:  # 显式给出pos和neg
+            query = input_tensor[target_tensor == 0, :]
+            positive_key = input_tensor[target_tensor == 1, :]
+            negative_keys = input_tensor[target_tensor == 2, :]
         else:
-            pos = target_tensor[0]==target_tensor
+            pos = target_tensor[0] == target_tensor
             query, positive_key = input_tensor[pos, :].chunk(2)
             if query.shape[0] != positive_key.shape[0]:
                 query = query[:-1]
             negative_keys = input_tensor[~pos, :]
 
-        return info_nce(query, positive_key, negative_keys,
-                        temperature=self.temperature,
-                        reduction=self.reduction,
-                        negative_mode=self.negative_mode) * self.alpha
+        return self.info_nce(query, positive_key, negative_keys, T=self.temperature) * self.alpha
+
+    def info_nce(self, query, pos, neg, T=0.1):
+        '''
+        :param query: [Nq, C]
+        :param pos: [Np, C]
+        :param neg: [Nn, C]
+        '''
+        query = F.normalize(query, dim=-1)
+        pos = F.normalize(pos, dim=-1)
+        neg = F.normalize(neg, dim=-1)
+
+        sim_pos = torch.mm(query, pos.transpose(0, 1))  # [Nq, Np]
+        sim_neg = torch.mm(query, neg.transpose(0, 1))  # [Nq, Nn]
+        logits = torch.cat([sim_pos, sim_neg], dim=1)
+        labels = torch.cat([torch.ones(sim_pos.shape[-1]), torch.zeros(sim_neg.shape[-1])]).to(query.device,
+                                                                                               dtype=query.dtype)
+        labels = repeat(labels, 'n -> Nq n', Nq=len(query))
+        return F.cross_entropy(logits / T, labels, reduction=self.reduction)
 
 
-def info_nce(query, positive_key, negative_keys=None, temperature=0.1, reduction='mean', negative_mode='unpaired'):
-    # Check input dimensionality.
-    if query.dim() != 2:
-        raise ValueError('<query> must have 2 dimensions.')
-    if positive_key.dim() != 2:
-        raise ValueError('<positive_key> must have 2 dimensions.')
-    if negative_keys is not None:
-        if negative_mode == 'unpaired' and negative_keys.dim() != 2:
-            raise ValueError("<negative_keys> must have 2 dimensions if <negative_mode> == 'unpaired'.")
-        if negative_mode == 'paired' and negative_keys.dim() != 3:
-            raise ValueError("<negative_keys> must have 3 dimensions if <negative_mode> == 'paired'.")
+class NoisyInfoNCELoss(InfoNCELoss):
+    def info_nce(self, query, pos, neg, T=0.1):
+        '''
+        :param query: [Nq, C]
+        :param pos: [Np, C]
+        :param neg: [Nn, C]
+        '''
+        query = F.normalize(query, dim=-1)
+        pos = F.normalize(pos, dim=-1)
+        neg = F.normalize(neg, dim=-1)
 
-    # Check matching number of samples.
-    if len(query) != len(positive_key):
-        raise ValueError('<query> and <positive_key> must must have the same number of samples.')
-    if negative_keys is not None:
-        if negative_mode == 'paired' and len(query) != len(negative_keys):
-            raise ValueError("If negative_mode == 'paired', then <negative_keys> must have the same number of samples as <query>.")
+        sim_pos = torch.mm(query, pos.transpose(0, 1))  # [Nq, Np]
+        sim_neg = torch.mm(query, neg.transpose(0, 1))  # [Nq, Nn]
+        logits = torch.cat([sim_pos, sim_neg], dim=1)
+        labels = torch.cat([torch.ones(sim_pos.shape[-1]), torch.zeros(sim_neg.shape[-1])]).to(query.device,
+                                                                                               dtype=query.dtype)
+        labels = repeat(labels, 'n -> Nq n', Nq=len(query))
 
-    # Embedding vectors should have same number of components.
-    if query.shape[-1] != positive_key.shape[-1]:
-        raise ValueError('Vectors of <query> and <positive_key> should have the same number of components.')
-    if negative_keys is not None:
-        if query.shape[-1] != negative_keys.shape[-1]:
-            raise ValueError('Vectors of <query> and <negative_keys> should have the same number of components.')
+        # RCE+NCE https://arxiv.org/pdf/2006.13554.pdf
+        rce = self.reverse_cross_entropy(logits / T, labels)
+        nce = self.normalized_cross_entropy(logits / T, labels)
+        return rce + nce
 
-    # Normalize to unit vectors
-    query, positive_key, negative_keys = normalize(query, positive_key, negative_keys)
-    if negative_keys is not None:
-        # Explicit negative keys
+    def reverse_cross_entropy(self, pred, labels):
+        pred = F.softmax(pred, dim=1)
+        pred = torch.clamp(pred, min=1e-7, max=1.0)
+        labels = torch.clamp(labels, min=1e-4, max=1.0)
+        rce = -torch.sum(pred * torch.log(labels), dim=1)
+        return rce
 
-        # Cosine between positive pairs
-        positive_logit = torch.sum(query * positive_key, dim=1, keepdim=True)
-
-        if negative_mode == 'unpaired':
-            # Cosine between all query-negative combinations
-            negative_logits = query @ transpose(negative_keys)
-
-        elif negative_mode == 'paired':
-            query = query.unsqueeze(1)
-            negative_logits = query @ transpose(negative_keys)
-            negative_logits = negative_logits.squeeze(1)
-
-        # First index in last dimension are the positive samples
-        logits = torch.cat([positive_logit, negative_logits], dim=1)
-        labels = torch.zeros(len(logits), dtype=torch.long, device=query.device)
-    else:
-        # Negative keys are implicitly off-diagonal positive keys.
-
-        # Cosine between all combinations
-        logits = query @ transpose(positive_key)
-
-        # Positive keys are the entries on the diagonal
-        labels = torch.arange(len(query), device=query.device)
-
-    return F.cross_entropy(logits / temperature, labels, reduction=reduction)
-
-
-def transpose(x):
-    return x.transpose(-2, -1)
-
-
-def normalize(*xs):
-    return [None if x is None else F.normalize(x, dim=-1) for x in xs]
+    def normalized_cross_entropy(self, pred, labels):
+        pred = F.log_softmax(pred, dim=1)
+        nce = -torch.sum(labels * pred, dim=1) / (-pred.sum(dim=1))
+        return nce
