97df1cace6dedc3288e9bdf686a65ca56430bb80
IrisRainbowNeko <228218809@qq.com>
2024-03-18 17:45:11 +0800

    rce+nce

--- rainbowneko/train/loss/__init__.py
+++ rainbowneko/train/loss/__init__.py
@@ -1,4 +1,4 @@
 from .base import LossContainer, LossGroup
 from .distillation import DistillationLoss
-from .contrastive import MLCEImageLoss, InfoNCELoss
+from .contrastive import MLCEImageLoss, InfoNCELoss, NoisyInfoNCELoss
 from .style import StyleLoss
