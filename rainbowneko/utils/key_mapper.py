9f5993cd22144cfc59efbdd0c3ae4fd4708f2cba
IrisRainbowNeko <228218809@qq.com>
2024-08-08 18:18:44 +0800

    update key mapper

--- rainbowneko/utils/key_mapper.py
+++ rainbowneko/utils/key_mapper.py
@@ -1,16 +1,40 @@
+from typing import Union, Iterable, Dict, Any
+
 
 class KeyMapper:
-    cls_map = {0: 'pred.pred', 1: 'target.label'}
+    cls_map = {0: 'pred.pred', 1: 'target.label'} # 'pred.pred -> 0', 'target.label -> 1'
     image_map = {0: 'pred.pred', 1: 'target.target_image'}
 
-    def __init__(self, host=None, key_map=None):
+    def __init__(self, host=None, key_map: Union[Iterable[str], Dict[Any, str]] = None):
         if key_map is None and host is not None:
             if hasattr(host, '_key_map'):
-                self.key_map = host._key_map
+                self.key_map = self.parse_key_map(host._key_map)
             else:
                 self.key_map = self.cls_map
         else:
-            self.key_map = key_map
+            self.key_map = self.parse_key_map(key_map)
+
+    def parse_key_map(self, key_map: Union[Iterable[str], Dict[Any, str]]):
+        if isinstance(key_map, dict):
+            return key_map
+        else:
+            key_map_parse = {}
+            for x in key_map:
+                src, dst = x.split('->')
+                src, dst = src.strip(), dst.strip()
+
+                try:
+                    src = int(src)
+                except:
+                    pass
+
+                try:
+                    dst = int(dst)
+                except:
+                    pass
+
+                key_map_parse[dst] = src
+            return key_map_parse
 
     def get_value(self, v, keys):
         for k in keys:
