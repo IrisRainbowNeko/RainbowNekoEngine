8f012d7e376eedb6bce150662c283e55cd9a748e
IrisRainbowNeko <228218809@qq.com>
2024-11-01 17:47:37 +0800

    yaml cfg

--- tests/train/one_gpu.py
+++ tests/train/one_gpu.py
@@ -5,12 +5,12 @@ import sys
 class PyCFGTester(TestCase):
     def test_multi_class_py(self):
         argv = sys.argv
-        sys.argv += ['--cfg', 'cfgs/py/train/classify/multi_class.py', 'train.train_steps=40', 'train.train_epochs=null']
+        sys.argv = sys.argv+['--cfg', 'cfgs/py/train/classify/multi_class.py', 'train.train_steps=40', 'train.train_epochs=null']
         neko_train()
         sys.argv = argv
 
     def test_multi_class_yaml(self):
         argv = sys.argv
-        sys.argv += ['--cfg', 'cfgs/yaml/train/classify/multi_class.yaml', 'train.train_steps=40', 'train.train_epochs=null']
+        sys.argv = sys.argv+['--cfg', 'cfgs/yaml/train/classify/multi_class.yaml', 'train.train_steps=40', 'train.train_epochs=null']
         neko_train()
         sys.argv = argv
