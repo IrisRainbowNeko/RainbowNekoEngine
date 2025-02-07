2dd6e8b41ad19f599b6fb6b33b3c95583f78b3b5
IrisRainbowNeko <228218809@qq.com>
2025-02-07 17:46:13 +0800

    lmdb dataset

--- rainbowneko/train/data/source/lmdb_img_label.py
+++ rainbowneko/train/data/source/lmdb_img_label.py
@@ -11,7 +11,6 @@ from .img_label import ImageLabelSource
 class LMDBImageLabelSource(ImageLabelSource):
     def __init__(self, img_root, label_file, repeat=1, **kwargs):
         super().__init__(img_root, label_file, repeat, **kwargs)
-
         self.env = lmdb.open(img_root, readonly=True, lock=False)  # 打开 LMDB 数据库
 
     def __getitem__(self, index) -> Dict[str, Any]:
