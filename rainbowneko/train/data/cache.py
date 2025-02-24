6b36723642106d8163f66c9046af10c18a742709
IrisRainbowNeko <rainbow-neko@outlook.com>
2025-02-24 14:26:08 +0800

    bug fix CacheableDataset

--- rainbowneko/train/data/cache.py
+++ rainbowneko/train/data/cache.py
@@ -57,7 +57,7 @@ class CacheableDataset(BaseDataset):
         self._disable = False
 
     def __getitem__(self, index):
-        if self._disable:
+        if self._disable or self.cache is None:
             return super().__getitem__(index)
         datas, full = self.cache.before_load(index)
         if full:
@@ -77,11 +77,13 @@ class CacheableDataset(BaseDataset):
     def batch_process(self, batch: Dict[str, Union[List, torch.Tensor]]):
         if self.batch_handler is not None:
             batch = self.batch_handler(batch)
-        batch = self.cache.on_batch(batch)
+        if self.cache is not None:
+            batch = self.cache.on_batch(batch)
         return batch
 
     def build_bucket(self, bs, world_size):
         self.bucket.build(bs=bs, world_size=world_size, source=self.source)
 
     def build_cache(self, model):
-        self.cache.build(self, model)
+        if self.cache is not None:
+            self.cache.build(self, model)
