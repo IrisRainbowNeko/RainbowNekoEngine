54d71e7ee8acab29b0659ad72b5833b68012bd5e
IrisRainbowNeko <228218809@qq.com>
2025-03-30 18:52:28 +0800

    bug fix; zero module

--- rainbowneko/utils/net.py
+++ rainbowneko/utils/net.py
@@ -83,3 +83,11 @@ def maybe_DDP(model):
         return model.module
     else:
         return model
+
+def zero_module(module):
+    """
+    Zero out the parameters of a module and return it.
+    """
+    for p in module.parameters():
+        p.detach().zero_()
+    return module
