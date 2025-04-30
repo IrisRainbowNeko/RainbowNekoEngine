9b46fb3be5bdfa88e7ad81f6d0b08fda7f165b0b
IrisRainbowNeko <228218809@qq.com>
2025-04-30 17:01:00 +0800

    _worker_info

--- rainbowneko/data/data_loader.py
+++ rainbowneko/data/data_loader.py
@@ -4,6 +4,8 @@ from queue import Empty
 from typing import Union, Iterable, Optional, Callable, List, Any, TypeVar
 
 from torch.utils.data import Sampler, IterableDataset, RandomSampler, SequentialSampler
+from torch.utils.data._utils import worker as torch_worker
+from torch.utils.data._utils.worker import WorkerInfo
 
 from .dataset import BaseDataset
 
@@ -13,7 +15,7 @@ _collate_fn_t = Callable[[List[T]], Any]
 
 class NekoDataLoader:
     def __init__(self, dataset: BaseDataset, batch_size=1, shuffle=False, sampler: Union[Sampler, Iterable, None] = None,
-                 num_workers: int = 0, collate_fn: Optional[_collate_fn_t] = None, generator=None,
+                 num_workers: int = 0, collate_fn: Optional[_collate_fn_t] = None, generator=None, drop_last: bool = False,
                  prefetch_factor: Optional[int] = 2, ):
         self.dataset = dataset
         self.batch_size = batch_size
@@ -21,6 +23,7 @@ class NekoDataLoader:
         self.num_workers = num_workers
         self.collate_fn = collate_fn
         self.generator = generator
+        self.drop_last = drop_last
         self.prefetch_factor = prefetch_factor
 
         if isinstance(dataset, IterableDataset):
@@ -34,10 +37,12 @@ class NekoDataLoader:
         self.sampler = sampler
 
     @staticmethod
-    def worker(worker_id, dataset, sampler, queue, queue_next, bs, prefetch_factor):
+    def worker(worker_id, num_workers, dataset, sampler, queue, queue_next, bs, prefetch_factor):
         """
         每个子进程执行的函数
         """
+        torch_worker._worker_info = WorkerInfo(id=worker_id, num_workers=num_workers, seed=42, dataset=dataset)
+
         batch = []
         batch_list = []
         for i, idx in enumerate(sampler):
@@ -60,10 +65,12 @@ class NekoDataLoader:
         queue.put((worker_id, None))
 
     @staticmethod
-    def worker_iter(worker_id, dataset, queue, queue_next, bs, prefetch_factor):
+    def worker_iter(worker_id, num_workers, dataset, queue, queue_next, bs, prefetch_factor):
         """
         每个子进程执行的函数
         """
+        torch_worker._worker_info = WorkerInfo(id=worker_id, num_workers=num_workers, seed=42, dataset=dataset)
+
         batch = []
         batch_list = []
         count = 0
@@ -77,6 +84,7 @@ class NekoDataLoader:
                     queue_next.get()
                     queue.put((worker_id, batch_list.pop(0)))
                     count -= 1
+
         queue.put((worker_id, None))
 
     def multi_process_iterate(self, dataset, num_workers=4, bs=64):
@@ -93,11 +101,11 @@ class NekoDataLoader:
 
         for worker_id in range(num_workers):
             if self.sampler == -1:
-                p = ctx.Process(target=self.worker_iter, args=(worker_id, datasets[worker_id], queue,
+                p = ctx.Process(target=self.worker_iter, args=(worker_id, num_workers, datasets[worker_id], queue,
                                                                queue_next_list[worker_id], bs // num_workers,
                                                                self.prefetch_factor))
             else:
-                p = ctx.Process(target=self.worker, args=(worker_id, datasets[worker_id], self.sampler, queue,
+                p = ctx.Process(target=self.worker, args=(worker_id, num_workers, datasets[worker_id], self.sampler, queue,
                                                           queue_next_list[worker_id], bs // num_workers, self.prefetch_factor))
             p.start()
             processes.append(p)
@@ -127,3 +135,6 @@ class NekoDataLoader:
 
     def __iter__(self):
         return self.multi_process_iterate(self.dataset, num_workers=self.num_workers, bs=self.batch_size)
+
+    def __len__(self):
+        return len(self.dataset)//self.batch_size
