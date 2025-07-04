af01b80fa038bebaa3cbada19f3b6347aab0801f
IrisRainbowNeko <rainbow-neko@outlook.com>
2025-07-04 18:04:34 +0800

    update

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 setuptools.setup(
     name="rainbowneko",
     py_modules=["rainbowneko"],
-    version="1.9.1",
+    version="1.9.2",
     author="IrisRainbowNeko",
     author_email="rainbow-neko@outlook.com",
     description="Neural network training and inference framework",
