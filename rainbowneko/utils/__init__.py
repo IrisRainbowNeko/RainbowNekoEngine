f8c7e50312e1af2f4f6bcfca153a21e6f72fc4cb
IrisRainbowNeko <rainbow-neko@outlook.com>
2025-07-14 17:09:13 +0800

    BatchableDict

--- rainbowneko/utils/__init__.py
+++ rainbowneko/utils/__init__.py
@@ -2,9 +2,10 @@ from .cfg_resolvers import *
 from .cfg_tool import change_num_classes
 from .constant import Path_Like, FILE_LIKE, weight_dtype_map
 from .key_mapper import KeyMapper
+from .module import disable_hf_loggers
+from .net import remove_layers, remove_all_hooks, to_cpu, to_cuda, hook_compile, split_module_name, xformers_available, maybe_DDP, zero_module, \
+    BatchableDict
+from .random import RandomContext
 from .scheduler import ConstantLR, CosineLR, CosineRestartLR, MultiStepLR, PolynomialLR, OneCycleLR, \
     get_scheduler_with_name, SchedulerType, ConstantWD, CosineWD, CosineRestartWD, MultiStepWD, PolynomialWD
-from .module import disable_hf_loggers
-from .net import remove_layers, remove_all_hooks, to_cpu, to_cuda, hook_compile, split_module_name, xformers_available, maybe_DDP
-from .random import RandomContext
 from .utils import *
