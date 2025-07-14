99a5cbb36439caf322c7202bd8a0c74f650426f9
IrisRainbowNeko <rainbow-neko@outlook.com>
2025-07-14 17:47:00 +0800

    BatchableDict

--- rainbowneko/utils/net.py
+++ rainbowneko/utils/net.py
@@ -116,3 +116,11 @@ class BatchableDict:
         else:
             super().__setattr__(name, value)
 
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.data})"
