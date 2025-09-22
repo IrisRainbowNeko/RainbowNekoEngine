7d2078b94494ffc9efc4ee9fee271f21de301079
IrisRainbowNeko <rainbow-neko@outlook.com>
2025-09-22 17:57:17 +0800

    ComposeWebdsSource with shuffle

--- rainbowneko/data/source/base.py
+++ rainbowneko/data/source/base.py
@@ -1,11 +1,12 @@
 import bisect
+import random
+from contextlib import contextmanager
+from itertools import chain
 from pathlib import Path
 from typing import Dict, List, Tuple, Any
 
 from rainbowneko.utils import Path_Like
 from rainbowneko.utils.img_size_tool import get_image_size
-from contextlib import contextmanager
-from itertools import chain
 
 
 class DataSource:
@@ -60,10 +61,12 @@ class ComposeDataSource(DataSource):
     def __len__(self):
         return self._offsets[-1]
 
+
 class ComposeWebdsSource(DataSource):
-    def __init__(self, source_list: List[DataSource]):
+    def __init__(self, source_list: List[DataSource], shuffle=False):
         self.source_list = source_list
         self.size = sum(len(source) for source in self.source_list)
+        self.shuffle = shuffle
 
         self._return_source = False
 
@@ -74,23 +77,39 @@ class ComposeWebdsSource(DataSource):
         self._return_source = False
 
     def __iter__(self):
-        self.source_iter = chain.from_iterable(
-            ((source, item) for item in source) for source in self.source_list
-        )
+        if self.shuffle:
+            self.source_iter_list = [iter(source) for source in self.source_list]
+        else:
+            self.source_iter = chain.from_iterable(
+                ((source, item) for item in source) for source in self.source_list
+            )
         return self
 
     def __next__(self):
-        source, item = next(self.source_iter)
+        if self.shuffle:
+            while True:
+                source_iter = random.choice(self.source_iter_list)
+                try:
+                    source, item = next(self.source_iter)
+                    break
+                except StopIteration:
+                    self.source_iter_list.remove(source_iter)
+                    if len(self.source_iter_list) == 0:
+                        raise StopIteration
+        else:
+            source, item = next(self.source_iter)
+
         if self._return_source:
             return item, source
         else:
-            return item 
+            return item
 
     def __getitem__(self, index) -> Dict[str, Any]:
         raise NotImplementedError('WebDatasetSource is not indexable')
 
     def __len__(self):
         return self.size
+
 
 class VisionDataSource(DataSource):
     def __init__(self, img_root: Path_Like, repeat=1, **kwargs):
